97c23a158fff5af93168f4279bebca2bd5963c49
Герасимов Степан Александрович <gerasimov@ws.redcom.ru>
2024-12-12 22:40:23 +1000

    Alert struct,  Recovery(), AccountReg(), TG

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 	"net/http"
     "time"
 	"os"
+	"strings"
 )
 
 /*
@@ -20,9 +21,16 @@ import (
 
 */
 
+type Alert struct {
+	Addr		string
+	Time 		time.Duration
+	Status		int
+	LogAlert 	bool
+}
+
 func main(){
 	for{
-		time.Sleep(5 * time.Second)
+		time.Sleep(1 * time.Second)
 		CheckingStatus(os.Getenv("REM_IP")+"/cgi-bin/sip_cgi?action=regstatus", 1)
 	}
 }
@@ -50,18 +58,16 @@ func CheckingStatus(ip string, timeout time.Duration) {
 		return
 	}
 
-    _, err = ioutil.ReadAll(resp.Body)
-
-	/*
-		!!! Добавить обработку ошибки чтения
-	*/
+    code, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-        log.Fatalln(err, "ioutil.ReadAll(resp.Body) Ошибка чтения body")
+        log.Println(err, "ioutil.ReadAll(resp.Body) Ошибка чтения body")
+		return
     }
 
 	switch resp.StatusCode {
 		case 200:
 			Status200(resp)
+			AccountReg(resp.Request.Host, string(code))
 		case 401:
 			Status401(resp)
 		case 404:
@@ -93,12 +99,49 @@ func CheckingStatus(ip string, timeout time.Duration) {
 
 
 
+/*
+	Восстановление оборудования после падения
+	проверка основных параметров 
+		Status: 200 (success)
+		SIP:      AccountReg1=1
+		Error:    10
+		Label:    200 key
+		ScanCode: 12345=>on
+		DoorCode: 15926=>on
+		Lock:     included
 
+*/
+func Recovery(){
+	var Status 	 	string
+	var SIP 	 	string
+	var Error 	 	string
+	var Label 	 	string
+	var ScanCode  	string
+	var DoorCode  	string
+	var Lock 	 	string
 
+	log.Println(Status, SIP, Error, Label, ScanCode, DoorCode, Lock)
+}
 
+/*
+	Проверка состояния SIP сервера
+*/
+func AccountReg(ip, status string){
+	//log.Println(status)
 
+	statuses := strings.Split(status, "\n")
 
+	if statuses[0] == "AccountReg1=1" {
+		log.Println(LevelLog("I"), ip, "AccountReg1=1")
+		return
+	}
 
+	if statuses[0] == "AccountReg1=0" {
+		log.Println(LevelLog("W"), ip, "AccountReg1=0")
+		return
+	}
+
+}
 func StatusTimeout(err error){
 	log.Println(LevelLog("E"), "Заданный узел не ответил в течении заданного {{timeout_second_request}}")
 	log.Println(LevelLog("E"), err)
@@ -145,6 +188,9 @@ func StatusNotDefined(resp *http.Response){
 	)
 }
 
+/*
+	Уровень логирования
+*/
 func LevelLog(level string) string {
 	switch level {
 		case "F":
@@ -163,3 +209,66 @@ func LevelLog(level string) string {
 			return ""
 	}
 }
+
+/*
+
+	ПРИМЕРЫ АЛЕРТОВ telegram
+
+*/
+
+// При статусе StatusTimeout
+/*
+❗ ERROR (StatusTimeout)
+	Addr: Груднова д.1 к.2 Под 1
+	Time: 12.12.24 (21:37:29)
+	Status: Not available
+	Reason: 
+	 - Заданный узел не ответил в течении заданного 
+	 - {{timeout_second_request}}
+*/
+
+// При статусе 401
+/*
+⚠️ Warning
+	Addr: Груднова д.1 к.2 Под 1
+	Time: 12.12.24 (21:37:29)
+	Status: 401
+	Reason: 
+	 - 401 Unauthorized - неверная авторизация для проверки статуса
+*/
+
+// При статусе 404
+/*
+⚠️ Warning
+	Addr: Груднова д.1 к.2 Под 1
+	Time: 12.12.24 (21:37:29)
+	Status: 404
+	Reason:
+	 - 404 Site or Page Not Found - указанная страница не существует
+	 - Проверьте правельность заполнения URL
+*/
+
+// При статусе 400
+/*
+⚠️ Warning
+	Addr: Груднова д.1 к.2 Под 1
+	Time: 12.12.24 (21:37:29)
+	Status: 400
+	Reason:
+	 - 400 Bad Request - Неверный переданный запрос
+	 - Проверьте правельность заполнения CGI в запросе
+*/
+
+// При статусе восстановлен
+/*
+✅ Restored
+	Addr:   Груднова д.1 к.2 Под 1
+	Time:   12.12.24 (21:37:29)
+	Status: 200
+		SIP:      AccountReg1=1
+		Error:    10
+		Label:    200 key
+		ScanCode: 12345=>on
+		DoorCode: 15926=>on
+		Lock:     included
+*/
